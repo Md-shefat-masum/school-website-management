88ef2e5f21e50d188c01506fa5cd86042635da78
Md-shefat-masum <mshefat924@gmail.com>
2023-10-06 02:10:53 +0600

    add role relation

--- app/Models/User.php
+++ app/Models/User.php
@@ -59,4 +59,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function roles()
+    {
+        return $this->belongsTo(Role::class,'role_sl','id');
+    }
 }
