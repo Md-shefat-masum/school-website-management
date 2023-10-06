d61d270c5323705c569fee453c596c47d7672f7e
Md-shefat-masum <mshefat924@gmail.com>
2023-10-07 02:46:27 +0600

    crate uplading image function

--- app/Http/helpers/common.php
+++ app/Http/helpers/common.php
@@ -1,6 +1,8 @@
 <?php
 
 use Carbon\Carbon;
+use Intervention\Image\Facades\Image;
+use Illuminate\support\Str;
 
 function set_token_coockie($token)
 {
@@ -29,4 +31,49 @@ function setting($key, $multiple = false)
     } catch (\Throwable $th) {
         var_dump($th->getMessage());
     }
+}
+
+/**
+ ```js
+    upload(
+    request()->file('file'), // single file source
+        "uploads/banners", // only folder name
+        400,
+        300,
+        "banner1", // file name without extension
+    )
+ */
+function upload($source, $path, $width = null, $height = null, $file_name = null)
+{
+    $image = Image::make($source);
+    // dd($source->getClientOriginalExtension(), $source->getClientOriginalName());
+    if (!$path) {
+        $path = public_path('uploads');
+        if (!file_exists($path)) {
+            mkdir($path, 0777, true);
+        }
+    } else {
+        if (!file_exists(public_path($path))) {
+            mkdir(public_path($path), 0777, true);
+        }
+    }
+
+    if (!$file_name) {
+        $file_name = Carbon::now()->toDateTimeString();
+        $file_name = Str::slug($file_name);
+        $file_name .= "." . $source->getClientOriginalExtension();
+    }else{
+        $file_name = Str::slug($file_name);
+    }
+
+    if ($width || $height) {
+        $image->resize($width, $height, function ($constraint) {
+            $constraint->aspectRatio();
+        });
+    }
+
+    $full_name = $path . '/' . $file_name;
+    $image->save(public_path($full_name));
+
+    return $full_name;
 }
