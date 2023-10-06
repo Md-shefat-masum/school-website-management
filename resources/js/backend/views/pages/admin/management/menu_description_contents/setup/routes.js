2f28cecc8b729394256112707db2fa0ed79d2683
Md-shefat-masum <mshefat924@gmail.com>
2023-10-07 04:04:29 +0600

    navbar menu setup routes

---
+++ resources/js/backend/views/pages/admin/management/menu_description_contents/setup/routes.js
@@ -0,0 +1,24 @@
+import setup from ".";
+import kebabize from "../../../../../../utils/kebabize";
+import Layout from "../Layout.vue";
+
+import Content from "../Content.vue";
+
+let route_prefix = setup.route_prefix;
+const routes =
+{
+    path: kebabize(route_prefix),
+    component: Layout,
+    name: route_prefix,
+    children: [
+        {
+            path: ":slug",
+            name: "Details",
+            component: Content,
+        },
+
+    ]
+};
+
+
+export default routes;
