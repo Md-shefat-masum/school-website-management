810e3a54f99b849d2675cb9a933b8959f76df2a4
Md-shefat-masum <mshefat924@gmail.com>
2023-10-07 04:03:42 +0600

    create form

---
+++ resources/js/backend/views/pages/admin/management/menu_description_contents/Content.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="">
+        <div class="card rounded-none">
+            <div class="card-header pt-3 pb-1">
+                <h4 class="bn" style="font-size: 22px!important;"> {{ data.title }} </h4>
+            </div>
+            <div class="card-body px-4 pt-4 form_area custom_scroll">
+                
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import SettingForm from './components/SettingForm.vue';
+export default {
+    components: { SettingForm },
+    data: () => ({
+        data: {},
+    }),
+    created: function(){
+        this.get_value();
+    },
+    methods: {
+        get_value: function(){
+            // console.log(this.$route.params);
+            axios.get('/navbar-menu-item/'+this.$route.params.slug)
+            .then(res=>{
+                console.log(res.data);
+                this.data = res.data;
+            })
+        },
+
+    }
+}
+</script>
+
+<style></style>
