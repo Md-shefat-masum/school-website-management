c0a6113bd2515587d2e4e12e5892f99a95972699
Md-shefat-masum <mshefat924@gmail.com>
2023-10-06 16:20:24 +0600

    manage redirection url

--- resources/js/backend/views/App.vue
+++ resources/js/backend/views/App.vue
@@ -10,13 +10,12 @@
 </template>
 
 <script>
-import SideBar from './components/sidebar/SideBar.vue'
 import TopHeader from './components/TopHeader.vue'
 import { mapActions, mapState } from 'pinia'
 import { use_auth_store } from "../store/auth_store"
 
 export default {
-    components: { TopHeader, SideBar },
+    components: { TopHeader },
     created: async function () {
         console.log('App Started');
         await this.check_is_auth();
@@ -26,10 +25,10 @@ export default {
             // console.log(v);
             switch (v.role_sl) {
                 case 1:
-                    this.$router.replace('super-admin')
+                    window.location.hash = window.location.hash?window.location.hash:"#/super-admin";
                     break;
                 case 2:
-                    this.$router.replace('admin')
+                    window.location.hash = window.location.hash?window.location.hash:"#/admin";
                     break;
                 default:
                     alert('you have no permission');
