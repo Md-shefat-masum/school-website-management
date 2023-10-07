d53714c739a78d3ab9721882b9ce9f0d63cb44c4
Md-shefat-masum <mshefat924@gmail.com>
2023-10-07 06:25:10 +0600

    add external content field

--- resources/js/backend/views/pages/admin/management/menu_description_contents/Content.vue
+++ resources/js/backend/views/pages/admin/management/menu_description_contents/Content.vue
@@ -13,26 +13,48 @@
                     </label>
                 </div>
             </div>
-            <div v-if="data.details" class="card-body px-4 py-2 form_area custom_scroll">
-                <editor api-key="no-api-key"
-                v-model="data.details.description"
-                :init="{
-                    height: 500,
-                    menubar: false,
-                    plugins: [
-                        'advlist autolink lists link image charmap print preview anchor',
-                        'searchreplace visualblocks code fullscreen',
-                        'insertdatetime media table paste code help wordcount'
-                    ],
-                    toolbar:
-                        'undo redo | formatselect | bold italic backcolor | \
-                           alignleft aligncenter alignright alignjustify | \
-                           bullist numlist outdent indent | removeformat | help'
-                }" />
-            </div>
-            <div class="card-footer py-2">
-                <button @click="update" class="btn btn-info">Submit</button>
-            </div>
+            <form @submit.prevent="update">
+                <div v-if="data.details" class="card-body px-4 py-2 form_area custom_scroll">
+                    <div class="row">
+                        <div class="col-lg-8">
+                            <editor api-key="no-api-key"
+                                v-model="data.details.description"
+                                :init="{
+                                    height: 500,
+                                    menubar: false,
+                                    plugins: [
+                                        'advlist autolink lists link image charmap print preview anchor',
+                                        'searchreplace visualblocks code fullscreen',
+                                        'insertdatetime media table paste code help wordcount'
+                                    ],
+                                    toolbar:
+                                        'undo redo | formatselect | bold italic backcolor | \
+                                        alignleft aligncenter alignright alignjustify | \
+                                        bullist numlist outdent indent | removeformat | help'
+                            }" />
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="form-group">
+                                <label for="">Source Title</label>
+                                <input type="text" v-model="data.details.source_title" name="source_title" class="form-control mt-1 mb-2">
+                            </div>
+                            <div class="form-group mt-3">
+                                <label for="">Source URL</label>
+                                <input type="text" v-model="data.details.source_url" name="source_url" class="form-control mt-1 mb-2">
+                                <a v-if="data.details.source_url" :href="data.details.source_url" target="_blank">source file url</a>
+                            </div>
+                            <div class="form-group mt-3">
+                                <label for="">Source File</label>
+                                <input type="file" name="source_file" class="form-control mt-1 mb-2">
+                                <a v-if="data.details.source_file" :href="'/'+data.details.source_file" target="_blank">source file link</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer py-2">
+                    <button class="btn btn-info">Submit</button>
+                </div>
+            </form>
         </div>
     </div>
 </template>
@@ -67,7 +89,10 @@ export default {
         },
         update: function () {
             // console.log(this.$route.params);
-            axios.post('/navbar-menu-details/update',{id: this.data.details.id, description: this.data.details.description})
+            let form_data = new FormData(event.target);
+            form_data.append('id',this.data.details.id);
+            form_data.append('description',this.data.details.description);
+            axios.post('/navbar-menu-details/update',form_data)
                 .then(res => {
                     // console.log(res.data);
                     // this.data = res.data;
