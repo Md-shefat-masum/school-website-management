6ebdc55541839f02e29db8f5fd4445883c6230dd
Md-shefat-masum <mshefat924@gmail.com>
2023-10-09 20:17:36 +0600

    notice routes

--- resources/js/backend/views/pages/admin/management/menu_description_contents/setup/routes.js
+++ resources/js/backend/views/pages/admin/management/menu_description_contents/setup/routes.js
@@ -3,6 +3,9 @@ import kebabize from "../../../../../../utils/kebabize";
 import Layout from "../Layout.vue";
 
 import Content from "../Content.vue";
+import All from "../All.vue";
+import Create from "../Create.vue";
+import Edit from "../Edit.vue";
 
 let route_prefix = setup.route_prefix;
 const routes =
@@ -16,6 +19,24 @@ const routes =
             name: "ContentPageDetails",
             props: true,
             component: Content,
+        },
+        {
+            path: ":slug/all",
+            name: "ContentPageDetailsAll",
+            props: true,
+            component: All,
+        },
+        {
+            path: ":slug/create",
+            name: "ContentPageDetailsCreate",
+            props: true,
+            component: Create,
+        },
+        {
+            path: ":slug/edit/:id",
+            name: "ContentPageDetailsEdit",
+            props: true,
+            component: Edit,
         },
 
     ]
