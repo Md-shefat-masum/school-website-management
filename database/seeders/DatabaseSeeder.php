89808f4be4216ce41a0aaf22fad0e68b8064c2d9
Md-shefat-masum <mshefat924@gmail.com>
2023-10-09 20:06:34 +0600

    update data base seeder and rearange

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,26 +16,39 @@ class DatabaseSeeder extends Seeder
 
             RoleSeeder::class,
             UserSeeder::class,
+
             AcademyCalssSeeder::class,
             AcademyCalssSubjectSeeder::class,
+
             AtAGlanceSeeder::class,
+
             BannerSeeder::class,
-            BlogCategorySeeder::class,
-            BlogSeeder::class,
+
             DigitalBookSeeder::class,
             DigitalBookCategorySeeder::class,
+
             EmployeeRoleSeeder::class,
             EmployeeSeeder::class,
+
             GalleryPhotoSeeder::class,
             GalleryPhotoCategorySeeder::class,
+
             GalleryPhotoSeeder::class,
             GalleryVideoCategorySeeder::class,
             GalleryVideoSeeder::class,
+
             NavbarMenuSeeder::class,
+            NoticeSeeder::class,
+
+            TagSeeder::class,
+
+            BlogCategorySeeder::class,
+            BlogSeeder::class,
+
             NewsCategorySeeder::class,
             NewsSeeder::class,
+
             SettingSeeder::class,
-            TagSeeder::class,
 
         ]);
     }
