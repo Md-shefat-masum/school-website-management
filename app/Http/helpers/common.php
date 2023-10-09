55f5f267e2b7389009dd3ba67ea9c89229cce4cb
Md-shefat-masum <mshefat924@gmail.com>
2023-10-09 20:06:21 +0600

    update file uploader for all types

--- app/Http/helpers/common.php
+++ app/Http/helpers/common.php
@@ -1,6 +1,7 @@
 <?php
 
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Storage;
 use Intervention\Image\Facades\Image;
 use Illuminate\support\Str;
 
@@ -46,35 +47,44 @@ function setting($key, $multiple = false)
  */
 function upload($source, $path, $width = null, $height = null, $file_name = null)
 {
-    $image = Image::make($source);
-    // dd($source->getClientOriginalExtension(), $source->getClientOriginalName());
-    if (!$path) {
-        $path = public_path('uploads');
-        if (!file_exists($path)) {
-            mkdir($path, 0777, true);
+    // dd($source->getClientMimeType(), $source->getClientOriginalExtension(), $source->getClientOriginalName());
+    $mime_type = $source->getClientMimeType();
+
+    if($mime_type != "image/png"){
+        $path = Storage::putFile($path,$source);
+        return $path;
+    }
+
+    if($mime_type == "image/png"){
+        $image = Image::make($source);
+        if (!$path) {
+            $path = public_path('uploads');
+            if (!file_exists($path)) {
+                mkdir($path, 0777, true);
+            }
+        } else {
+            if (!file_exists(public_path($path))) {
+                mkdir(public_path($path), 0777, true);
+            }
         }
-    } else {
-        if (!file_exists(public_path($path))) {
-            mkdir(public_path($path), 0777, true);
+
+        if (!$file_name) {
+            $file_name = Carbon::now()->toDateTimeString();
+            $file_name = Str::slug($file_name);
+            $file_name .= "." . $source->getClientOriginalExtension();
+        } else {
+            $file_name = Str::slug($file_name);
         }
+
+        if ($width || $height) {
+            $image->resize($width, $height, function ($constraint) {
+                $constraint->aspectRatio();
+            });
+        }
+
+        $full_name = $path . '/' . $file_name;
+        $image->save(public_path($full_name));
+        return $full_name;
     }
 
-    if (!$file_name) {
-        $file_name = Carbon::now()->toDateTimeString();
-        $file_name = Str::slug($file_name);
-        $file_name .= "." . $source->getClientOriginalExtension();
-    } else {
-        $file_name = Str::slug($file_name);
-    }
-
-    if ($width || $height) {
-        $image->resize($width, $height, function ($constraint) {
-            $constraint->aspectRatio();
-        });
-    }
-
-    $full_name = $path . '/' . $file_name;
-    $image->save(public_path($full_name));
-
-    return $full_name;
 }
