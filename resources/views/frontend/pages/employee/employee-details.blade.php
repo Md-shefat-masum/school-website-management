59e7e894678cc32af0c6c0f101815eaee0dacf4f
Md-shefat-masum <mshefat924@gmail.com>
2023-10-10 23:07:59 +0600

    employee details page

---
+++ resources/views/frontend/pages/employee/employee-details.blade.php
@@ -0,0 +1,24 @@
+@extends('frontend.layouts.website')
+@section('contents')
+
+<section class="news_area inner_news_and_blog_details_page aos-init aos-animate" data-aos="fade-up"
+    data-aos-duration="1000">
+
+    <div class="container">
+        <div class="news_content news_and_blog_content">
+            <!-- page_title_area start -->
+            <div class="page_title_area">
+                <h2 class="page_title">
+                    শিক্ষক
+                </h2>
+            </div>
+            <!-- page_title_area end -->
+
+            <div class="">
+                শিক্ষক
+            </div>
+
+        </div>
+    </div>
+</section>
+@endsection
