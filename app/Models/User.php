2f7b870a5510adc24e62e3f2c2bf31b4abd3e0c1
Md-shefat-masum <mshefat924@gmail.com>
2023-12-14 01:09:28 +0600

    users related relation

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,9 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Models\Student\StudentDue;
+use App\Models\Student\StudentInformation;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -64,4 +67,10 @@ class User extends Authenticatable
     {
         return $this->belongsTo(Role::class,'role_sl','id');
     }
+
+    public function information()
+    {
+        return $this->hasOne(StudentInformation::class,'student_id','id');
+    }
+
 }
